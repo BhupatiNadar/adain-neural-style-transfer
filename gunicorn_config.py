1a6c6e0bb5417db851fc12ff0dcdd84f5053f7e5
BhupatiNadar <tamillana2@gmail.com>
2026-06-28 22:30:23 +0530

    removed config file

--- gunicorn_config.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-     
-workers = 1
-worker_class = "uvicorn.workers.UvicornWorker"  # lighter than sync
-worker_connections = 10
-max_requests = 100
-max_requests_jitter = 10
-timeout = 30
